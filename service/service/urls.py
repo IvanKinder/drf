b9dec550cc3b951e69c312d57cdd556e0c0c5e56
Ivan Kinder <ivankinder11@gmail.com>
2021-03-02 14:42:58 +0300

    rest begin

--- service/service/urls.py
+++ service/service/urls.py
@@ -6,9 +6,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from library.views import AuthorModelViewSet
+from userapp.views import UserModelViewSet
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
+router.register('users', UserModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),

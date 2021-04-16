6d319518d55aa0c49f13c2b47cab38b017b0b28b
Ivan Kinder <ivankinder11@gmail.com>
2021-04-16 12:58:44 +0300

    l10

--- service/service/schema.py
+++ service/service/schema.py
@@ -17,9 +17,20 @@ class AuthorType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
+    all_authors = graphene.List(AuthorType)
+    author_by_uuid = graphene.Field(AuthorType, uuid=graphene.UUID(required=True))
 
     def resolve_all_books(root, info):
         return Book.objects.all()
+
+    def resolve_all_authors(root, info):
+        return Author.objects.all()
+
+    def resolve_author_by_uuid(root, info, uuid):
+        try:
+            return Author.objects.get(uuid=uuid)
+        except Author.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)

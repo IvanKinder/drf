bae883dac9836f381519b659df038eece5dbfba2
Ivan Kinder <ivankinder11@gmail.com>
2021-04-16 13:09:47 +0300

    l10

--- service/service/schema.py
+++ service/service/schema.py
@@ -19,6 +19,7 @@ class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
     all_authors = graphene.List(AuthorType)
     author_by_uuid = graphene.Field(AuthorType, uuid=graphene.UUID(required=True))
+    books_by_author_name = graphene.List(BookType, name=graphene.String(required=False))
 
     def resolve_all_books(root, info):
         return Book.objects.all()
@@ -31,6 +32,16 @@ class Query(graphene.ObjectType):
             return Author.objects.get(uuid=uuid)
         except Author.DoesNotExist:
             return None
+
+    def resolve_books_by_author_name(root, info, name=None):
+        if name:
+            try:
+                author_uuid = Author.objects.get(last_name=name)
+                return Book.objects.filter(author=author_uuid)
+            except Book.DoesNotExist:
+                return None
+        else:
+            return Book.objects.all()
 
 
 schema = graphene.Schema(query=Query)

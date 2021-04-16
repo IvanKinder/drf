e1f1980402a77fb7f692e709e9a3411fedc68206
Ivan Kinder <ivankinder11@gmail.com>
2021-04-16 13:40:14 +0300

    l10

--- service/service/schema.py
+++ service/service/schema.py
@@ -1,6 +1,26 @@
 import graphene
 from graphene_django import DjangoObjectType
 from library.models import Book, Author
+from todo.models import ToDo, Project
+from django.contrib.auth.models import User
+
+
+class TodoType(DjangoObjectType):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
+
+
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class BookType(DjangoObjectType):
@@ -39,6 +59,14 @@ class Query(graphene.ObjectType):
     all_authors = graphene.List(AuthorType)
     author_by_uuid = graphene.Field(AuthorType, uuid=graphene.UUID(required=True))
     books_by_author_name = graphene.List(BookType, name=graphene.String(required=False))
+    all_todo = graphene.List(TodoType)
+    all_projects = graphene.List(ProjectType)
+
+    def resolve_all_todo(root, info):
+        return ToDo.objects.all()
+
+    def resolve_all_projects(root, info):
+        return Project.objects.all()
 
     def resolve_all_books(root, info):
         return Book.objects.all()
